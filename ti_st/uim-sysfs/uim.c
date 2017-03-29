a2fe7ecdb877a519b327be1165ea9405c4381646
Gary Bisson <gary.bisson@boundarydevices.com>
2017-03-29 15:08:01 +0200

    uim-sysfs: update kim device search mechanism
    Now looking for any device with "kim" in it, since our 4.1.15 device
    trees use bt_kim as name for it to be more explicit (and in order)
    in the file.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- ti_st/uim-sysfs/uim.c
+++ ti_st/uim-sysfs/uim.c
@@ -668,7 +668,7 @@ bdaddr_t *strtoba(const char *str)
 // ignore the "." and ".." entries
 static int dir_filter(const struct dirent *name)
 {
-    if (0 == strncmp("kim.", name->d_name, 4))
+    if (NULL != strstr(name->d_name, "kim"))
             return 1;
 
     return 0;

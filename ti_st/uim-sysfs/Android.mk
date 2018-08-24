828e479ab284188061ba71c798c27a7b024a09c3
Gary Bisson <gary.bisson@boundarydevices.com>
2018-08-24 16:58:30 +0200

    ti_st: uim-sysfs: fix tool path to be in /vendor for Oreo
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- ti_st/uim-sysfs/Android.mk
+++ ti_st/uim-sysfs/Android.mk
@@ -13,6 +13,10 @@ LOCAL_SRC_FILES:= \
 	uim.c
 LOCAL_CFLAGS:= -g -c -W -Wall -O2 -D_POSIX_SOURCE
 LOCAL_SHARED_LIBRARIES:= libnetutils libcutils liblog
+ifeq ($(shell test $(MAJOR_VERSION) -ge 8 && echo true), true)
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+endif
 LOCAL_MODULE:=uim-sysfs
 LOCAL_MODULE_TAGS:= eng
 include $(BUILD_EXECUTABLE)
